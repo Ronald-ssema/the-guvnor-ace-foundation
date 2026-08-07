0d2e250f80323e6b3021f426a1427d2d7a1933d0
Ronald Ssema <Ronald-ssema@users.noreply.github.com>
2026-08-07 20:59:38 +0100

    Restore accessibility main landmark

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,11 +91,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body>
         <Navbar />
 
-        {children}
+        <main id="main-content">{children}</main>
 
         <Footer />
 
